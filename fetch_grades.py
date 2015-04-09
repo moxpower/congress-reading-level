2cc07f6d79b6867967f009b6fd642eddb41b55ea
pfarnach <tefl.pat@gmail.com>
2015-04-09 09:56:44 -0700

    Populated DB with scores for most politicians

--- fetch_grades.py
+++ fetch_grades.py
@@ -1,17 +1,20 @@
 import requests
 import urllib2
+import json
 from pprint import pprint
-from models import Politician
+from models import Politician, db
 from keys import sunlight_key
 
-pols = Politician.query.all()
-count = 0
+# get all politicians in DB
+pols = Politician.query.filter_by(index_ari=8.33).all()
+count_good = 0
+count_bad = 0
 
+# analyze each politician by bioguide ID
 for pol in pols:
 	bioguide_id = pol.bioguide_id
-	print bioguide_id
-	count += 1
 	
+	# get speeches from Capitol Words API
 	query_params = { 'apikey': sunlight_key,
 					 'per_page': 100,
 					 'bioguide_id': bioguide_id
@@ -21,28 +24,45 @@ for pol in pols:
 
 	response = requests.get(endpoint, params=query_params)
 	data = response.json()
-	# pprint.pprint(data['results'])
-	# pprint.pprint(len(data['results']))
-	# pprint.pprint(data['results'][4]['speaking'])
-	lines = data['results'][4]['speaking']
 
-	speech = ""
-	for line in lines:
-		speech += line + " "
+	# concat speech strings into single string to be analyzed
+	all_speeches = ""
+	for result in data['results']:
+		for speech in result['speaking']:
+			if len(all_speeches + " " + speech) <= 86100:
+				all_speeches += speech + " "
+			else:
+				break
 
-	print speech
-	print len(speech)
+	print len(all_speeches)
+	all_speeches = all_speeches.replace('\\', '')
 
-	values = '{"async": false,"data": {"content": "'+ speech +'",}}'
+	# prepare and send speech to priceonomics API
+	values = """{"async": false,"data": {"content": "'+ %s +'"}}""" % all_speeches
 
 	headers = {
 	  'X-Access-Key': '34ed16e6b5f645c5a3cba39c75ee80b2'
 	}
-	request = urllib2.Request('https://api.engine.priceonomics.com/v1/apps/readinglevel', data=values, headers=headers)
 
-	response_body = urllib2.urlopen(request).read()
-	pprint(response_body)
+	try:
+		request = urllib2.Request('https://api.engine.priceonomics.com/v1/apps/readinglevel', data=values, headers=headers)
+		response_body = urllib2.urlopen(request)
+		response_json = json.load(response_body)
 
-	if count >= 1:
-		break
+		pol.index_ari = response_json['data'].get('ari')
+		pol.index_coleman = response_json['data'].get('coleman-liau')
+		pol.index_smog = response_json['data'].get('smog')
+		pol.index_gunning = response_json['data'].get('gunning-fog')
+		pol.index_flesch = response_json['data'].get('flesch-kincaid')
+		pol.index_composite = response_json['data'].get('composite')
+		db.session.commit()
+		count_good += 1
+		print "Successful"
+	except urllib2.HTTPError as e:
+		count_bad += 1
+		print "Failed"
+
+print count_good, count_bad
+print "Successful {0}, Failed {1}".format(count_bad, count_bad)
+
 
